a3ee2aa0db1d47810c7ab1371bd5e685ce4db70a
Christian Schlichtherle <christian@schlichtherle.de>
2018-04-12 19:38:47 +0400

    Improves `DeltaSpec`.

--- delta/src/test/scala/global/namespace/archive/io/delta/DeltaSpec.scala
+++ delta/src/test/scala/global/namespace/archive/io/delta/DeltaSpec.scala
@@ -6,7 +6,6 @@ package global.namespace.archive.io.delta
 
 import java.nio.charset.Charset
 import java.security.MessageDigest
-import java.util.logging.{Level, Logger}
 
 import global.namespace.archive.io.delta.Delta._
 import global.namespace.archive.io.delta.DeltaSpec._
@@ -23,13 +22,13 @@ import scala.collection.JavaConverters._
 class DeltaSpec extends WordSpec {
 
   "A delta model" should {
-    "support round-trip encoding/decoding to/from a sink/source" in {
-      forAll(TestCases) { builder =>
-        val original = builder.messageDigest(sha1).build
+    "support round-trip encoding/decoding to/from JSON" in {
+      forAll(TestCases) { (builder, json) =>
+        val original = (builder messageDigest sha1).build
         val store = memoryStore
         encodeModel(store, original)
+        utf8String(store) shouldBe json
         val clone = decodeModel(store)
-        logger.log(Level.FINE, "\n{0}", utf8String(store))
         clone shouldBe original
         clone should not be theSameInstanceAs(original)
       }
@@ -41,17 +40,20 @@ private object DeltaSpec {
 
   import DeltaModel.{builder => b}
   val TestCases = Table(
-    "delta model builder",
-    b,
-    b.changedEntries(List(new EntryNameAndTwoDigestValues("changed", "1", "2")).asJava),
-    b.addedEntries(List(new EntryNameAndDigestValue("added", "1")).asJava),
-    b.removedEntries(List(new EntryNameAndDigestValue("removed", "1")).asJava),
-    b.unchangedEntries(List(new EntryNameAndDigestValue("unchanged", "1")).asJava)
+    ("delta model builder", "JSON string"),
+    (b,
+      """{"algorithm":"SHA-1"}"""),
+    (b changedEntries List(new EntryNameAndTwoDigestValues("changed", "1", "2")).asJava,
+      """{"algorithm":"SHA-1","changed":[{"name":"changed","first":"1","second":"2"}]}"""),
+    (b addedEntries List(new EntryNameAndDigestValue("added", "1")).asJava,
+      """{"algorithm":"SHA-1","added":[{"name":"added","digest":"1"}]}"""),
+    (b removedEntries List(new EntryNameAndDigestValue("removed", "1")).asJava,
+      """{"algorithm":"SHA-1","removed":[{"name":"removed","digest":"1"}]}"""),
+    (b unchangedEntries List(new EntryNameAndDigestValue("unchanged", "1")).asJava,
+      """{"algorithm":"SHA-1","unchanged":[{"name":"unchanged","digest":"1"}]}""")
   )
 
   val sha1: MessageDigest = MessageDigest getInstance "SHA-1"
-
-  val logger: Logger = Logger.getLogger(getClass.getName)
 
   def utf8String(store: Store): String = new String(store.content, utf8)
 
